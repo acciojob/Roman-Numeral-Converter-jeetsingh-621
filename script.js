94bd1abe303cf531c22f982c681d9ce4f354344d
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-12-01 11:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 function convertToRoman(num) {
+    // Define the Roman numeral symbols and their corresponding values, including subtractive combinations
     const romanSymbols = [
         ['M', 1000], 
+        ['CM', 900], 
         ['D', 500], 
+        ['CD', 400], 
         ['C', 100], 
+        ['XC', 90], 
         ['L', 50], 
+        ['XL', 40],
         ['X', 10], 
+        ['IX', 9], 
         ['V', 5], 
+        ['IV', 4], 
         ['I', 1]
     ];
 
     let result = '';
 
+    // Loop through the Roman symbols array
     for (let i = 0; i < romanSymbols.length; i++) {
+        // Get the symbol and its value
         const [symbol, value] = romanSymbols[i];
         
+        // Determine how many times the Roman numeral can fit into the number
         while (num >= value) {
+            // Append the symbol to the result string
             result += symbol;
+            // Subtract the value from the number
             num -= value;
         }
     }
@@ -24,6 +36,7 @@ function convertToRoman(num) {
 }
 
 // Test cases
+console.log(convertToRoman(14));   // Output: XIV
 console.log(convertToRoman(36));   // Output: XXXVI
 console.log(convertToRoman(798));  // Output: DCCXCVIII
 
